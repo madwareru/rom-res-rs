17df3b4d14adf9b8181f259c0c08ed0bed86dd8a
madwareru <madware.ru@gmail.com>
2020-09-28 12:47:54 +0300

    Minor fixes

--- examples/print_game_text_content.rs
+++ examples/print_game_text_content.rs
@@ -7,9 +7,7 @@ fn main() {
     let cursor = Cursor::new(MAIN_RES);
     if let Ok(resource_file) = ResourceFile::new(cursor) {
         let mut resource_file = resource_file;
-        if let Ok(txt_file) =
-            resource_file.get_resource_bytes("text/battle/m10/event01.txt"){
-            let mut bytes = txt_file;
+        if let Ok(bytes) = resource_file.get_resource_bytes("text/battle/m10/event01.txt") {
             let decoded_text = cp866_rs::decode_bytes(bytes);
             println!("{}", decoded_text);
         }
