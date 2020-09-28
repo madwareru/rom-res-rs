c53565696dc8d6c06843551c29af77662ed0c8cc
madwareru <madware.ru@gmail.com>
2020-09-28 15:29:37 +0300

    Minor fixes

--- examples/play_music.rs
+++ examples/play_music.rs
@@ -3,6 +3,7 @@ use std::io::Cursor;
 use quad_snd::*;
 use quad_snd::mixer::SoundMixer;
 use wav::BitDepth;
+use std::alloc::handle_alloc_error;
 
 const MUSIC_RES: &[u8] = include_bytes!("MUSIC.RES");
 
@@ -10,25 +11,39 @@ fn main() {
     let cursor = Cursor::new(MUSIC_RES);
     if let Ok(resource_file) = ResourceFile::new(cursor) {
         let mut resource_file = resource_file;
-        if let Ok(bytes) = resource_file.get_resource_bytes("inn.wav") {
-            let (header, data) = wav::read(&mut(&bytes[..])).unwrap();
-            let mut new_vec = Vec::new();
-            match data {
-                BitDepth::Eight(d) => {assert!(false);}
-                BitDepth::Sixteen(d) => {
-                    for i_16 in d {
-                        new_vec.push(i_16 / 2);
-                        new_vec.push(i_16 / 2);
+        if let Ok(bytes) = resource_file.get_resource_bytes("b07.wav") {
+            let (header, old_track) = wav::read(&mut(&bytes[..])).unwrap();
+
+            let track = match old_track {
+                BitDepth::Eight(d) => {
+                    let mut new_vec = Vec::new();
+                    for u_8 in d {
+                        new_vec.push(u_8);
+                        new_vec.push(u_8);
                     }
-                }
-                BitDepth::TwentyFour(d) => {assert!(false);}
-                BitDepth::Empty => {assert!(false);}
-            }
+                    BitDepth::Eight(new_vec)
+                },
+                BitDepth::Sixteen(d) => {
+                    let mut new_vec = Vec::new();
+                    for i_16 in d {
+                        new_vec.push(i_16);
+                        new_vec.push(i_16);
+                    }
+                    BitDepth::Sixteen(new_vec)
+                },
+                BitDepth::TwentyFour(d) => {
+                    let mut new_vec = Vec::new();
+                    for i_32 in d {
+                        new_vec.push(i_32);
+                        new_vec.push(i_32);
+                    }
+                    BitDepth::TwentyFour(new_vec)
+                },
+                BitDepth::Empty => BitDepth::Empty
+            };
             let mut header = header;
-            header.sampling_rate = 22050 * 2;
-            header.bytes_per_sample = 4;
-            header.bytes_per_second = 88200 * 2;
-            let track = BitDepth::Sixteen(new_vec);
+            header.sampling_rate = header.sampling_rate * 2;
+            header.bytes_per_second = header.bytes_per_second * 2;
 
             let mut reprocessed_audio: Vec<u8> = Vec::new();
 
